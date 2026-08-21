a53e4dd4a0a945cbfed3d135bad413bae49b7908
Aditya Bhalerao <adityabhalerao.tech@gmail.com>
2026-08-21 16:30:46 +0530

    Enlarge the easter egg hit area

--- src/components/EasterEgg.jsx
+++ src/components/EasterEgg.jsx
@@ -1,10 +1,10 @@
 const EasterEgg = () => (
-  <div className="fixed bottom-6 right-6 z-50 hidden lg:block">
+  <div className="fixed bottom-3 right-3 z-50 hidden lg:block">
     <a
       href="https://youtu.be/rEq1Z0bjdwc?si=FmUBM5WYfjhE8PFX"
       target="_blank"
       rel="noopener noreferrer"
-      className="select-none text-[10px] italic text-muted hover:text-brand"
+      className="inline-block select-none p-3 text-[10px] italic text-muted hover:text-brand"
       style={{ cursor: `url('/egg/light-saber.cur'), pointer` }}
     >
       hello there
